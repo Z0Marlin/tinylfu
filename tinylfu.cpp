d363000a9b433d42ea5dc262b8273a19d1b16d81
Tanmai <vvijaytanmai@gmail.com>
2019-04-09 20:17:50 +0530

    add refresh()

--- tinylfu.cpp
+++ tinylfu.cpp
@@ -4,23 +4,33 @@
 
 class TinyLFU {
 private:
-    uint32_t steps;
+    uint32_t sample_size, counter;
     size_t size;
     CachePolicy *cache_policy;
     BloomFilter::BloomFilter *histogram;
 
 public:
-    TinyLFU(size_t size=1e3, CachePolicy *policy=NULL, uint32_t steps=10) {
+    TinyLFU(size_t size=1e3, CachePolicy *policy=NULL, uint32_t sample_size=100) {
         if (policy == NULL) {
             throw std::invalid_argument("No Cache policy Supplied");
         }   
 
-        this->steps = steps;
+        this->counter = 0;
+        this->sample_size = sample_size;
         this->size = size;
         this->cache_policy = policy;
         this->histogram = new BloomFilter::BloomFilter(size);
     }
 
+    void refresh() {
+        if(counter == sample_size) {
+            for(cacheobj_t i = 0; i < histogram->tsize(); i++) {
+                histogram->setValue(i, histogram->getValue(i)>>2);
+            }
+            counter = 0;
+        }
+        return ;
+}
     void add(cacheobj_t i) {
         if (cache_policy->has_obj(i))
             return;
@@ -28,6 +38,7 @@ public:
         if (cache_policy->is_full() == false) {
             cache_policy->add(i);
             histogram->Add(i);  
+            
         }
         else {
             cacheobj_t victim = cache_policy->next_victim();
@@ -37,6 +48,10 @@ public:
                 cache_policy->add(i);
             }               
         }
-    }
-}; 
 
+        counter++;
+        refresh();
+    }
+
+
+}; 
